079e174918080ecf64ea97a7a7a07ac12fd0bb16
Lee Seungeun <seungeun8307@gmail.com>
2026-03-11 12:41:51 +0900

    BOJ 1003 피보나치 풀이

--- BOJ/seungeun8307/1003.py
+++ BOJ/seungeun8307/1003.py
@@ -1,6 +1,8 @@
-T = int(input())
+import sys
+
+T = int(sys.stdin.readline())
 for _ in range(T):
-    N = int(input())
+    N = int(sys.stdin.readline())
     a, b = 1, 0
     for i in range(N):
         a,b = b, a+b 
