9659c77226e67c5a2618b0a94c64351ab56c6bd4
Lee Seungeun <seungeun8307@gmail.com>
2026-03-21 10:46:38 +0900

    SQL: 131115 문제 풀이

---
+++ BOJ/seungeun8307/14626.py
@@ -0,0 +1,18 @@
+import sys
+
+IBSN = sys.stdin.readline()
+m = int(IBSN[-2])
+
+ibsnI = []
+ind = 0
+for i in range(13):
+    if IBSN[i]=='*' : ibsnI.append(IBSN[i]);ind=i
+    else: ibsnI.append(int(IBSN[i])) 
+
+for i in range(13):
+    if i==ind : continue
+    elif i%2!=0 : m += ibsnI[i]
+    else: m += ibsnI[i]*3
+
+a = m%10 -1
+print(a)
