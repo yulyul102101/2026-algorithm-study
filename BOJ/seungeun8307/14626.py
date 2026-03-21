413f657575f1481260c8896c149a4c060ed83fa6
Lee Seungeun <seungeun8307@gmail.com>
2026-03-21 11:25:25 +0900

    BOJ: 14626 문제 풀이

--- BOJ/seungeun8307/14626.py
+++ BOJ/seungeun8307/14626.py
@@ -1,20 +1,23 @@
-'''
 import sys
 
-IBSN = sys.stdin.readline()
-m = int(IBSN[-2])
+IBSN = sys.stdin.readline().strip()
 
 ibsnI = []
 ind = 0
 for i in range(13):
-    if IBSN[i]=='*' : ibsnI.append(IBSN[i]);ind=i
-    else: ibsnI.append(int(IBSN[i])) 
+    if IBSN[i] == '*':
+        ibsnI.append('*')
+        ind = i
+    else: ibsnI.append(int(IBSN[i]))
 
+m = 0
 for i in range(13):
-    if i==ind : continue
-    elif i%2!=0 : m += ibsnI[i]
-    else: m += ibsnI[i]*3
+    if i == ind: continue
+    if i % 2 == 0: m += ibsnI[i]
+    else: m += ibsnI[i] * 3
 
-a = m%10 -1
-print(a)
-'''
+for j in range(10):
+    tmp = j * (1 if ind % 2 == 0 else 3) + m
+    if tmp % 10 == 0:
+        print(j)
+        break
