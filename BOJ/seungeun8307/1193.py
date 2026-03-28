1d404bf878455bc49eed3d168c33ff235984fb90
Lee Seungeun <seungeun8307@gmail.com>
2026-03-28 11:49:51 +0900

    BOJ: 1193 문제 풀이

--- BOJ/seungeun8307/1193.py
+++ BOJ/seungeun8307/1193.py
@@ -1,4 +1,14 @@
 import sys
 
 X = int(sys.stdin.readline())
+line = 1
 
+while X > line: #대각선 길이 하나씩 늘려가면서 대각선 몇개인지 세기
+    X -= line
+    line += 1
+
+#a가 분자 b가 분모
+if line%2 == 0: a=X ; b=line-X+1
+else: a=line-X+1 ; b=X
+
+print(f'{a}/{b}')
