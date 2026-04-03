ae81af646c39c42cf656b800d12703f7b0bd98a9
Lee Seungeun <seungeun8307@gmail.com>
2026-04-03 16:45:40 +0900

    BOJ: 10101 문제 풀이

---
+++ BOJ/seungeun8307/10101.py
@@ -0,0 +1,10 @@
+import sys
+
+a1 = int(sys.stdin.readline())
+a2 = int(sys.stdin.readline())
+a3 = int(sys.stdin.readline())
+
+if(a1+a2+a3 != 180): print('Error')
+elif(a1 == a2 == a3 == 60): print('Equilateral')
+elif(a1 == a2 or a1 == a3 or a2 == a3): print('Isosceles')
+else: print('Scalence')
