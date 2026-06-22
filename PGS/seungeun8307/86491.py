46ac066ee4af3253e0302bab0218d938e8689970
Lee Seungeun <seungeun8307@gmail.com>
2026-06-22 15:13:48 +0900

    PGS: 86491 문제 풀이

---
+++ PGS/seungeun8307/86491.py
@@ -0,0 +1,11 @@
+def solution(sizes):
+    answer = 0
+    mw, mh = 0, 0
+    for w, h in sizes:
+        if w<h:
+            w, h = h,w
+        mw = max(mw, w)
+        mh = max(mh, h)
+    
+    answer = mw*mh
+    return answer
