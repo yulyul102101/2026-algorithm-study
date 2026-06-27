0929b84af58667b16c51fd06a153efeb2efbc4b2
Lee Seungeun <seungeun8307@gmail.com>
2026-06-27 21:35:26 +0900

    PGS: 120841 문제 풀이

---
+++ PGS/seungeun8307/120841.py
@@ -0,0 +1,7 @@
+def solution(dot):
+    answer = 0
+    if dot[0]>0 and dot[1]>0: answer=1
+    elif dot[0]<0 and dot[1]>0: answer=2
+    elif dot[0]>0 and dot[1]<0: answer=4
+    else: answer=3
+    return answer
