a4e3c8366ba36127a6335f7a26d64119ca9263d4
Lee Seungeun <seungeun8307@gmail.com>
2026-08-03 14:18:53 +0900

    PGS: 181830 문제 풀이

---
+++ PGS/seungeun8307/181830.py
@@ -0,0 +1,12 @@
+def solution(arr):
+    r, c = len(arr), len(arr[0])
+    
+    if r>c:
+        for i in range(r):
+            arr[i] += [0]*(r-c)
+            
+    elif c > r:
+        for _ in range(c-r):
+            arr.append([0]*c)
+                
+    return arr
